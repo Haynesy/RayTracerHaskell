a54ac058c4a42f68bd55e0db0b1ba204b3531bba
Adam Haynes <haynesy.adam@gmail.com>
2016-07-24 15:06:01 +1000

    Add rakefile and fix compilation

--- App.hs
+++ App.hs
@@ -1,7 +1,10 @@
+width = 200
+height = 100
+
 main :: IO ()
 main = do 
     createImage "Test.ppm" width height
     return ()
 
 createImage :: String -> Int -> Int -> IO ()
-createImage = undefined
+createImage str width height = putStrLn "Yay!"
---
+++ Rakefile
@@ -0,0 +1,6 @@
+APP_ENTRY_POINT = 'App.hs'
+
+desc 'Run'
+task :run do
+    system "runhaskell #{APP_ENTRY_POINT}"
+end
